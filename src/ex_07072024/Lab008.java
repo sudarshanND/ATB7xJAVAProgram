268803b1dc137ddffb4fea0420b4085323e868c3
Sudarshan <suddev3@gmail.com>
2024-08-15 13:02:48 +0530

    Added 1 new file

---
+++ src/ex_07072024/Lab008.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        System.out.println("trying to commit and add the files to git");
+        System.out.println("trying");
+        System.out.println("trying to ");
+        System.out.println("trying to commit");
+        System.out.println("trying to commit and add the files to git");
+    }
+}
